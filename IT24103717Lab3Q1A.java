c38ae55d06ef7551ad688f7c030f6bd7a83d3815
IT24103717 <it24103717@my.sliit.lk>
2024-11-01 13:22:31 +0530

    Add files via upload
    IT24103717Lab3Q1A Submitted

---
+++ IT24103717Lab3Q1A.java
@@ -0,0 +1,24 @@
+public class IT24103717Lab3Q1A{
+
+    public static void main(String[] args){
+    
+         int priceof1kgrice= 225;
+
+         int thenumberofkilogramswanted= 5;
+
+         float priceof5kgrice=(priceof1kgrice * thenumberofkilogramswanted);
+
+System.out.println("Enter the price of 1kg of rice: " +priceof1kgrice);
+
+System.out.println("Enter the number of kilograms you want to buy: " +thenumberofkilogramswanted);
+
+System.out.println();
+
+System.out.println("The total amount is: " +priceof5kgrice);
+
+
+
+    }
+}
+
+         
