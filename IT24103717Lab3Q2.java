f974117aa5db4ad4342464695beb94e773a2d49c
IT24103717 <it24103717@my.sliit.lk>
2024-11-06 15:44:22 +0530

    Add files via upload
    IT24103717Lab3Q2 Submitted

---
+++ IT24103717Lab3Q2.java
@@ -0,0 +1,31 @@
+public class IT24103717Lab3Q2{
+
+    public static void main(String[] args){
+
+       int monthlysalary= 50000;
+
+       int othourlyrate=1000;
+
+       int numberofothours=5;
+
+       double otamount=(othourlyrate*numberofothours);
+
+       double totalsalary=(monthlysalary+(othourlyrate*numberofothours));
+
+System.out.println("Enter the monthly salary:" +monthlysalary);
+
+System.out.println("Enter the number of OT hours:" +numberofothours);
+System.out.println("Enter the OT hourly rate:" +othourlyrate);
+
+System.out.println();
+
+System.out.println("The total salary including OT is:" +totalsalary);
+      }
+
+}
+
+       
+
+       
+           
+    
