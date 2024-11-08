6451b760e0d4b6518ed1f96331d4792989de3cc1
IT24103717 <it24103717@my.sliit.lk>
2024-11-08 07:24:33 +0530

    IT24103717Lab3Q2.java

--- IT24103717Lab3Q2.java
+++ IT24103717Lab3Q2.java
@@ -5,13 +5,13 @@ public class IT24103717Lab3Q2{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         
-     System.out.println("Enter the monthly salary: ");
+     System.out.print("Enter the monthly salary: ");
        double monthlysalary= input.nextDouble();
         
-     System.out.println("Enter the number of OT hours: ");
+     System.out.print("Enter the number of OT hours: ");
       double numberofothours= input.nextDouble();
 
-     System.out.println("Enter the OT hourly rate: ");
+     System.out.print("Enter the OT hourly rate: ");
        double othourlyrate= input.nextDouble();
 
        double otamount=(othourlyrate*numberofothours);
@@ -20,7 +20,7 @@ public class IT24103717Lab3Q2{
 
      System.out.println();
 
-    System.out.println("The total salary including OT is: " +totalsalary);
+    System.out.print("The total salary including OT is: " +totalsalary);
       }
 
 }
