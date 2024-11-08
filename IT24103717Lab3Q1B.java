c4f5db38ad36ecded82c2923290e2d7f2c68da87
IT24103717 <it24103717@my.sliit.lk>
2024-11-08 07:28:32 +0530

    Update IT24103717Lab3Q1B.java

--- IT24103717Lab3Q1B.java
+++ IT24103717Lab3Q1B.java
@@ -5,11 +5,11 @@ public class IT24103717Lab3Q1B{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the price of 1kg of rice: ");
+        System.out.print("Enter the price of 1kg of rice: ");
 
            double priceof1kgrice= input.nextDouble();
         
-        System.out.println("Enter the number of kilograms you want to buy: ")
+        System.out.print("Enter the number of kilograms you want to buy: ")
         
           double thenumberofkilogramswanted= input.nextDouble();
 
@@ -19,7 +19,9 @@ public class IT24103717Lab3Q1B{
  
            double totalamountwithdiscount=(priceofkgrice-discount);
         
-        System.out.println("The total amount after 10% discount is: " + totalamountwithdiscount);
+        System.out.println();
+        
+        System.out.print("The total amount after 10% discount is: " + totalamountwithdiscount);
         
 
          
